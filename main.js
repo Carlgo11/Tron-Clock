bb21c65672dd657c22019d633b18dc8b21c015b5
Carlgo11 <Carlgo11@users.noreply.github.com>
2019-04-15 03:49:21 +0200

    Clean up some code

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     startTime();
 });
 
 function startTime() {
-    let date = new Date();
+    const date = new Date();
 
-    let y = parse(date.getFullYear().toString().slice(2,4));
-    let m = parse(date.getMonth());
-    let d = parse(date.getDate());
-    let h = parse(date.getHours());
-    let i = parse(date.getMinutes());
-    let s = parse(date.getSeconds());
+    const y = parse(date.getFullYear().toString().slice(2, 4));
+    const m = parse(date.getMonth());
+    const d = parse(date.getDate());
+    const h = parse(date.getHours());
+    const i = parse(date.getMinutes());
+    const s = parse(date.getSeconds());
 
     document.getElementById('clock').innerHTML = y + ":" + m + ":" + d + ":" + h + ":" + i + ":" + s;
-    setTimeout(startTime, 1000); // Update every second (1000 ms)
+    // Update every 0.5 seconds (500 ms)
+    setTimeout(startTime, 500);
 }
 
 function parse(value) {
-    return value.toString().padStart(2, '0'); // Add a leading zero to numbers less than ten.
+    // Add a leading zero to numbers less than ten.
+    return value.toString().padStart(2, '0');
 }
